c6ce1fff4584f59ef0f325cdd519a8f360a8df7c
Israel <israel@esparza.com>
2022-05-04 22:58:22 -0600

    Add project files.

---
+++ BooksWebApp.Data/Dtos/BookDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksWebApp.Data.Dtos
+{
+    public class BookDto
+    {
+        public long? Isbn { get; set; }
+        public string Title { get; set; }
+        public string Subtitle { get; set; }
+        public string Author { get; set; }
+        public string Published { get; set; }
+        public string Publisher { get; set; }
+        public string Pages { get; set; }
+        public string Description { get; set; }
+        public string Website { get; set; }
+        public string Reviews { get; set; }
+        public int? Stars { get; set; }
+    }
+}
---
+++ BooksWebApp.UI/DataAccess/BooksAccess.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksWebApp.UI.DataAccess
+{
+    public static class BooksAccess
+    {
+
+    }
+}
